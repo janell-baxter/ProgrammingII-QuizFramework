f293f8a272ebe46126c3c806e9cae4111494714d
Janell Baxter <jbaxter@colum.edu>
2023-03-06 10:48:46 -0600

    Add project files.

---
+++ ProgrammingII-QuizFramework/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProgrammingII_QuizFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Start();
+        }
+    }
+}
