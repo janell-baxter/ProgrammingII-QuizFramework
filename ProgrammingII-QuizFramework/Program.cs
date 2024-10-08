096b1a981e765cc0b46bdea6b43e747c3bcf0a55
Janell Baxter <jbaxter@colum.edu>
2024-10-07 20:34:57 -0500

    Update Program.cs
    Added title for title bar

--- ProgrammingII-QuizFramework/Program.cs
+++ ProgrammingII-QuizFramework/Program.cs
@@ -1,8 +1,8 @@
 ﻿/*
  * Programming II Quiz Framework
  * YourName, Date
- * credits: 
- * Built on a framework provided in class by janellbaxter.com
+ * credits: 
+ * Modified and extended code provided in class: https://github.com/janell-baxter/ProgrammingII-QuizFramework 
  */
 
 namespace ProgrammingII_QuizFramework
@@ -11,6 +11,7 @@ namespace ProgrammingII_QuizFramework
     {
         static void Main(string[] args)
         {
+            Console.Title = "";
             Engine engine = new Engine();
             engine.Start();
         }
